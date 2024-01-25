8056f70e1b1dd6eaa84f9f2ee508eb1115f5607e
mikluxa21 <teterevkolya21@gmail.com>
2024-01-25 20:48:10 +0300

    methods Interface.h to Interface.cpp

--- Work/src/CCSDSParser/Interface.h
+++ Work/src/CCSDSParser/Interface.h
@@ -7,62 +7,11 @@ template <class TypeInterface>
 class Interface
 {
 public:
-	Interface()
-	{
-		for(size_t i = 0; i <=31; i++)
-			{
-				std::vector<unsigned char> vec;
-				vecResult.push_back(vec);
-			}
-	}
+	Interface();
 	
-	void addVCDU(TypeInterface& networkPackage, size_t packageLen)
-	{
-		ParentVCDU* parentVCDU = new VCDU<TypeInterface>(networkPackage, packageLen);
-		if(this->prevFrameCounter == -1)
-			this->prevFrameCounter = parentVCDU->getToUlongFrameCounter();
-		else
-		{
-			if((parentVCDU->getToUlongFrameCounter() - this->prevFrameCounter) != 1)
-				throw std::runtime_error("violation of personnel order");
-			this->prevFrameCounter = parentVCDU->getToUlongFrameCounter();
-		}
-		
-		std::vector<unsigned char> lastCCSDSPackage = parentVCDU->getLastCCSDSPacksge();
-		if(lastCCSDSPackage.size() != 0)
-			{
-				if(this->lastAPID == -1)
-					throw std::runtime_error("undefined end of CCSDS package");
-				else
-				{
-					this->vecResult[this->lastAPID].insert(vecResult[this->lastAPID].end(), lastCCSDSPackage.begin(), lastCCSDSPackage.end());
-				}
-			}
-		
-		if(parentVCDU->getToUlongVCID() != 63)
-			{
-				for(auto ccsds: parentVCDU->getCCSDSVector())
-					{
-						if(ccsds->getToUlongAPID() <= 31)
-						{
-							this->lastAPID = ccsds->getToUlongAPID();
-							auto resEndIter = this->vecResult[this->lastAPID].end();
-							std::vector<unsigned char> payloadVCDU = ccsds->getPayload();
-							auto ccsdsBeginIter = payloadVCDU.begin();
-							auto ccsdsEndIter = payloadVCDU.end();
-							this->vecResult[this->lastAPID].insert(resEndIter, ccsdsBeginIter, ccsdsEndIter);
-						}
-					}
-			}
-		delete parentVCDU;
-	}
+	void addVCDU(TypeInterface& , size_t );
 public:
-	std::vector<std::vector<unsigned char>> getCCSDSPackages() 
-	{
-		if(this->prevFrameCounter == -1)
-			throw std::runtime_error("Empty vecResilt");
-		return this->vecResult;
-	}
+	std::vector<std::vector<unsigned char>> getCCSDSPackages();
 private:
 	size_t prevFrameCounter = -1;
 	size_t lastAPID = -1;
--- Work/src/CCSDSParser/src/Interface.cpp
+++ Work/src/CCSDSParser/src/Interface.cpp
@@ -1,1 +1,61 @@
 #include <Interface.h>
+
+template <class TypeInterface>
+Interface<TypeInterface>::Interface()
+{
+	for(size_t i = 0; i <=31; i++)
+		{
+			std::vector<unsigned char> vec;
+			vecResult.push_back(vec);
+		}
+}
+
+template <class TypeInterface>
+void Interface<TypeInterface>::addVCDU(TypeInterface& networkPackage, size_t packageLen)
+{
+	ParentVCDU* parentVCDU = new VCDU<TypeInterface>(networkPackage, packageLen);
+	if(this->prevFrameCounter == -1)
+		this->prevFrameCounter = parentVCDU->getToUlongFrameCounter();
+	else
+	{
+		if((parentVCDU->getToUlongFrameCounter() - this->prevFrameCounter) != 1)
+			throw std::runtime_error("violation of personnel order");
+		this->prevFrameCounter = parentVCDU->getToUlongFrameCounter();
+	}
+	
+	std::vector<unsigned char> lastCCSDSPackage = parentVCDU->getLastCCSDSPacksge();
+	if(lastCCSDSPackage.size() != 0)
+		{
+			if(this->lastAPID == -1)
+				throw std::runtime_error("undefined end of CCSDS package");
+			else
+			{
+				this->vecResult[this->lastAPID].insert(vecResult[this->lastAPID].end(), lastCCSDSPackage.begin(), lastCCSDSPackage.end());
+			}
+		}
+	
+	if(parentVCDU->getToUlongVCID() != 63)
+		{
+			for(auto ccsds: parentVCDU->getCCSDSVector())
+				{
+					if(ccsds->getToUlongAPID() <= 31)
+					{
+						this->lastAPID = ccsds->getToUlongAPID();
+						auto resEndIter = this->vecResult[this->lastAPID].end();
+						std::vector<unsigned char> payloadVCDU = ccsds->getPayload();
+						auto ccsdsBeginIter = payloadVCDU.begin();
+						auto ccsdsEndIter = payloadVCDU.end();
+						this->vecResult[this->lastAPID].insert(resEndIter, ccsdsBeginIter, ccsdsEndIter);
+					}
+				}
+		}
+	delete parentVCDU;
+}
+
+template <class TypeInterface>
+std::vector<std::vector<unsigned char>> Interface<TypeInterface>::getCCSDSPackages() 
+{
+	if(this->prevFrameCounter == -1)
+		throw std::runtime_error("Empty vecResilt");
+	return this->vecResult;
+}
